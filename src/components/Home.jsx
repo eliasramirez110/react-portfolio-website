e07d34fdd3243646454f8444d2e671450e2c891b
Elias Ramirez <ramirezelias110@gmail.com>
2022-07-14 03:16:17 -0400

    home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {HiArrowNarrowRight} from 'react-icons/hi'
+
+const Home = () => {
+  return (
+    <div name='home' className='w-full h h-screen bg-[#0a192f]'>
+      {/* Container */}
+      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+        
+        <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Elias Ramirez</h1>
+        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>Full Stack Engineer</h2>
+        <p className='text-[#8892b0] py-4 max-w-[700px]'>I'm a full-stack engineer specializing in front-end development. Currently I'm focused on building responsive web applications using React.js </p>
+        <div>
+          <button className='text-white border-2 px-6 py-3 my-2 flex items-center'>
+          View Work 
+          <span className='hover:rotate-90 duration-300'>
+          <HiArrowNarrowRight className='ml-3'/>
+          </span> 
+          </button>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Home
