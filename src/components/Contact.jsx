b10d8aead6dc16102ba6ad0b53a77656f8694840
Elias Ramirez <ramirezelias110@gmail.com>
2022-07-17 23:56:38 -0400

    all components

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
         <div className='pb-8 bg-[#0a192f]'>
           <p className='text-4xl font-bold inline border-b-4 text-gray-300'>Contact</p>
           <p className='text-gray-300 py-4 '>Can't wait to connect with you!</p>
-        </div> 
+        </div>  
         <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
         <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
         <textarea className='bg-[#ccd6f6] p-2' name="message" rows="10" placeholder='Message'></textarea>
